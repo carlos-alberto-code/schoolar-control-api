d406557796ff5b9075747485d3ae4bb065ce937f
Carlos Baltazar <A840159943@my.uvm.edu.mx>
2025-01-03 12:33:33 -0600

    Agregar configuración de base de datos MySQL y modelos de usuario y rol. Inicialización de alembic

--- schoolar_control_api/database/connection.py
+++ schoolar_control_api/database/connection.py
@@ -1,9 +1,28 @@
+import os
+from dotenv import load_dotenv
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database.models import Base
 
 
-engine = create_engine('sqlite:///schoolar_control.db')
+load_dotenv()
 
+HOST = os.getenv('HOST')
+USER_NAME = os.getenv('USER_NAME')
+PASSWORD = os.getenv('PASSWORD')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
 
-def get_session(): ...
+DATABASE_URL = f'mysql+mysqlconnector://{USER_NAME}:{PASSWORD}@{HOST}/{DATABASE_NAME}'
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+@contextmanager
+def get_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- schoolar_control_api/database/models.py
+++ schoolar_control_api/database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Integer, String, ForeignKey, Column, Table
+from datetime import datetime
+from sqlalchemy import Integer, String, ForeignKey, Column, Table, DATE, TIMESTAMP, TIME, CheckConstraint
 from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 
 
@@ -6,4 +7,46 @@ class Base(DeclarativeBase):
     pass
 
 
+class Role(Base):
+    __tablename__ = 'roles'
+    __table_args__ = (
+        CheckConstraint('LENGTH(name) > 3', name='check_role_name_length')
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+
+    users: Mapped['User'] = relationship('User', secondary='roles_users', back_populates='roles')
+
+    def __repr__(self):
+        return f'Role(id={self.id!r}, name={self.name!r})'
+
+
+class User(Base):
+    __tablename__ = 'users'
+    __table_args__ = (
+        CheckConstraint('LENGTH(username) > 3', name='check_username_length'),
+        CheckConstraint('LENGTH(password) > 6', name='check_password_length'),
+        CheckConstraint(r"email ~* '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'", name='check_email_format')
+    )
+
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    fullname: Mapped[str] = mapped_column(String(100), unique=False, nullable=False)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(100), unique=False, nullable=False)
+
+    roles: Mapped[Role] = relationship('Role', secondary='roles_users', back_populates='users')
+
+    def __repr__(self):
+        return f'User(id={self.id!r}, fullname={self.fullname!r}, username={self.username!r}, email={self.email!r})'
+
+
+roles_users = Table(
+    'roles_users',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('role_id', Integer, ForeignKey('roles.id')),
+)
 
